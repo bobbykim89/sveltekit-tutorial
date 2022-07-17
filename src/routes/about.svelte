54bd948262038cd46689658e79976fd908f88a5b
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-07-17 01:50:05 -0700

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<div class="about">
+  <h2>About Us</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad id nesciunt
+    atque suscipit provident est earum vel eaque neque assumenda, error facilis
+    laborum reiciendis minus eius, pariatur voluptate asperiores delectus
+    praesentium incidunt. Ut, pariatur odit. Velit, ducimus asperiores
+    praesentium quibusdam at cumque nisi et suscipit placeat odio nulla tempora
+    molestias.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+    distinctio voluptas voluptates, provident voluptatibus quaerat rem fugiat
+    non, deleniti quam impedit accusamus. Nulla, corporis distinctio sunt modi
+    fuga quasi exercitationem.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
